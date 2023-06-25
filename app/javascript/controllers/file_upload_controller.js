d31a5f04e28309941cc43eb807324aea8b70fc19
Thomas Cosse <tomcosse123@gmail.com>
2023-06-25 12:36:01 +0200

    added input file button

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -3,5 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="file-upload"
 export default class extends Controller {
   connect() {
+    console.log("file uploader")
+  }
+
+  fileLoaded() {
+    console.log('file loaded')
+    console.log(this.element.file)
+
+    fetch()
   }
 }
